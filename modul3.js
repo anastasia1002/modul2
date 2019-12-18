d4af8961ea358566652a63595bec447a90eef43f
anastasia1002 <56667810+anastasia1002@users.noreply.github.com>
2019-12-18 12:08:22 +0200

    Add files via upload

---
+++ modul3.js
@@ -0,0 +1,54 @@
+let forums=[
+    {
+        topic:"школа",
+        short:"відвідуваність школи",
+        data:"10липня",
+        count_discussion:5;  
+    },
+    {
+        topic:"університет",
+        short:"відвідуваність університету",
+        data:"17грудня",
+        count_discussion:8;  
+    },
+    {
+        topic:"булінг",
+        short:"булінг в дитячому віці",
+        data: "28квітня",
+        count_discussion: 3;  
+    },
+    {
+        topic:"шкидливі звички",
+        short:"куріння і наркотики",
+        data:"30березня",
+        count_discussion:1,  
+    },
+    {
+        topic:"безпека",
+        short:"безпека на дорозі",
+        data:"18грудня",
+        count_discussion:8,
+    },
+]
+
+/*var tbl = document.getElementById('tbl');
+
+var tr = '<tr>';
+for(var i = 0; i <= forum.length; i++)
+    tr += '<th></th>';
+tr += '</tr>';
+for(var j = 0; j <= forum.length; j++)
+    tbl.innerHTML += tr;
+
+var th = tbl.getElementsByTagName('th');
+for(i = 0; i < forum.length; i++)
+    th[i + 1].innerHTML = forum[i];
+
+for(let forum of forums){
+    forum.topic =0 ;
+    for(let data of forum.datas){
+        forum.topic+=data;
+    }
+}
+alert(forum)*/
+
